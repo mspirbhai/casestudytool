fb9bde3174586a3b94c6885724a5e0b9794032e9
mspirbhai <mustafaspirbhai@gmail.com>
2023-06-10 14:01:12 +0300

    added tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,6 @@
-from django.test import TestCase
-from django.test import SimpleTestCase
-
-# Create your tests here.
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class HomepageTests(SimpleTestCase):
@@ -9,8 +8,38 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
 
-class AboutpageTests(SimpleTestCase):
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<title>Home page</title>")
+
+
+class AboutpageTests(TestCase):
     def test_url_exists_at_correct_location_and_redirects_to_login(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 302)
+
+    def test_url_available_by_name_and_redirects_to_login(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_template_name_correct(self):
+        User = get_user_model()
+        user = User.objects.create_user("Mustafa," "mustafa@dev.io", "some_pass")
+        self.client.force_login(user)
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "pages/about.html")
+
+    def test_template_content(self):
+        User = get_user_model()
+        user = User.objects.create_user("Mustafa," "mustafa@dev.io", "some_pass")
+        self.client.force_login(user=user)
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
