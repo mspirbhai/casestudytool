8df06f71e8e31552ded88c366f2e2dbee26e2f01
mspirbhai <mustafaspirbhai@gmail.com>
2023-06-10 13:13:08 +0300

    add tests and ignore static files

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location_and_redirects_to_login(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 302)
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 
-class AboutPageView(TemplateView):
+class AboutPageView(LoginRequiredMixin, TemplateView):
     template_name = "pages/about.html"
