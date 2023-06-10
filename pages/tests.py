5f531b7704d6aae78c447b94f080d434a74a9675
mspirbhai <mustafaspirbhai@gmail.com>
2023-06-10 22:56:22 +0300

    refactors and added tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,8 +1,7 @@
 from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from pages.models import Case
-from accounts.models import CustomUser
+from pages.models import Case, CaseLog
 
 
 class HomepageTests(SimpleTestCase):
@@ -20,8 +19,8 @@ class HomepageTests(SimpleTestCase):
 class AboutpageTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = CustomUser.objects.create_user(
-            "Mustafa," "mustafa@dev.io", "some_pass"
+        cls.user = get_user_model().objects.create_user(
+            username="Mustafa", email="mustafa@dev.io", password="some_pass"
         )
 
     def test_url_exists_at_correct_location_and_redirects_to_login(self):
@@ -39,10 +38,10 @@ class AboutpageTests(TestCase):
 class CasepageTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = CustomUser.objects.create_user(
-            "Mustafa," "mustafa@dev.io", "some_pass"
+        cls.user = get_user_model().objects.create_user(
+            username="Mustafa", email="mustafa@dev.io", password="some_pass"
         )
-        cls.case = Case.objects.create(case_name="Test Case")
+        cls.case = Case.objects.create(case_name="Test Case", description="Descripton")
 
     def test_url_exists_at_correct_location_and_redirects_to_login(self):
         response = self.client.get("/cases/")
@@ -53,5 +52,48 @@ class CasepageTests(TestCase):
         response = self.client.get(reverse("cases"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/cases.html")
-        self.assertContains(response, "<h1>Show all cases for Logged in User</h1>")
-        self.assertContains(response, "Test Case created at")
+        self.assertContains(response, "<h1>Show All Cases For Logged In User</h1>")
+        self.assertContains(response, '<h5 class="mb-1">Test Case</h5>')
+
+
+class CaseLogsPageTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="Mustafa", email="mustafa@dev.io", password="some_pass"
+        )
+        cls.case = Case.objects.create(case_name="Test Case", description="Descripton")
+        cls.caselog = CaseLog.objects.create(
+            title="Test Case Log 1",
+            case_name=cls.case,
+            author=cls.user,
+            body="Test Case Log 1 Body Text",
+        )
+
+    def test_cases_url_exists_at_correct_location_and_redirects_to_login(self):
+        response = self.client.get("/cases/1/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_caselog_list_with_pk_1_logged_in(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("caselogs", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/caselogs.html")
+        self.assertContains(response, "<h1>Show all case logs for Logged in User</h1>")
+        self.assertContains(response, '<h5 class="mb-1">Test Case Log 1</h5>')
+
+    def test_caselogs_url_exists_at_correct_location_and_redirects_to_login(self):
+        response = self.client.get("/caselogs/1/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_caselog_detail_with_pk_1_logged_in(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("caselog_detail", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/caselog_detail.html")
+        self.assertContains(
+            response, "<h1>Show Details for Case log Test Case for Logged in User</h1>"
+        )
+        self.assertContains(
+            response, "<h2>Test Case Log 1 written by mustafa@dev.io</h2>"
+        )
