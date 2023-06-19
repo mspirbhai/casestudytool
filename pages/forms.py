73ef49a689c7cc397c8a7b0cfb1f39bb5b6a90e2
mspirbhai <mustafaspirbhai@gmail.com>
2023-06-19 11:37:39 +0300

    forms.py cleanup

--- pages/forms.py
+++ pages/forms.py
@@ -9,11 +9,10 @@ class CaseLogCreateForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
-        super(CaseLogCreateForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         # self.fields["title"].initial = "Test"
         case_number = int(self.request.path.split("/")[-2:-1].pop())
         project = Case.objects.get(pk=case_number).project
         project_tracked_metrics = TrackedMetric.objects.filter(project=project)
         self.fields["tracked_metric"].queryset = project_tracked_metrics
         # self.fields["author"].disabled = True
-        # self.fields["tracked_metric"].queryset = Case.objects.filter
